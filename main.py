0007ad19762f8bacf625db0dc7f05c32f934065a
odedig <Sinister_2097>
2018-06-12 20:02:20 +0100

    Initial set and publish

--- main.py
+++ main.py
@@ -18,9 +18,25 @@ def translate(text=None,model_id=None):
         data='Error: text was not translated'
         return data
 
+def langs (lang=None):
+    lang = lang.lower()
+    availablelang = {
+        'german': 'de',
+        'french':'fr',
+        'italian':'it',
+        'korean':'ko',
+        'spanish':'es',
+        'arabic':'ar'
+    }
+    try:
+        lang_code = (availablelang[lang])
+        return lang_code
 
+    except KeyError:
+        print('Unsupported Lang')
 
 if __name__ == '__main__':
     text = input('Translate what ?')
-    translate_output=translate(text = text,model_id='en-es')
+    model_id = 'en-'+ langs(input('To what lanuage ?'))
+    translate_output=translate(text = text,model_id=model_id)
     print(translate_output['translations'][0]['translation'])
